b48f0348481f608b8624eeb2ca090a01e415d848
christian.sonic <yoh.chrisitan@gmail.com>
2023-04-28 20:04:28 -0400

    Initial Commit

---
+++ FashionUI/FashionUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FashionUIApp.swift
+//  FashionUI
+//
+//  Created by Sonic on 28/4/23.
+//
+
+import SwiftUI
+
+@main
+struct FashionUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
